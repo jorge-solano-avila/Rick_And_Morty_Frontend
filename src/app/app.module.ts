8e7f7b15bcaaf5d7962872e51665536eea8033d0
Jorge Solano <jorgesolavila@gmail.com>
2019-03-11 18:36:58 -0500

    Feat: update app module
    Add Material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,23 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { SeasonsComponent } from "./seasons/seasons.component";
+import { SeasonsComponent } from "./components/seasons/seasons.component";
+import { SeasonComponent } from "./components/season/season.component";
 
 @NgModule({
-  declarations: [AppComponent, SeasonsComponent],
+  declarations: [AppComponent, SeasonsComponent, SeasonComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
