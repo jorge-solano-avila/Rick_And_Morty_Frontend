0a5ef2be619538c7ceb590790835747b0fd86d44
Jorge Solano <jorgesolavila@gmail.com>
2019-03-13 18:59:30 -0500

    Feat: update episode component
    Update initial component

--- src/app/components/episode/episode.ts
+++ src/app/components/episode/episode.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+
+import { EpisodeData, Episode } from "src/app/models/episode/episode";
+import { EpisodeService } from "../../services/episode/episode";
+import { AppGlobal } from "../../app.global";
 
 @Component({
   selector: "app-episode",
@@ -6,7 +11,32 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./episode.scss"]
 })
 export class EpisodeComponent implements OnInit {
-  constructor() {}
+  episode: Episode;
+  isLoading: boolean;
 
-  ngOnInit() {}
+  constructor(
+    private route: ActivatedRoute,
+    private episodeService: EpisodeService
+  ) {
+    this.episode = null;
+    this.isLoading = true;
+  }
+
+  private getEpisode(id: number): void {
+    this.episodeService.getById(id)
+      .subscribe((response: EpisodeData) => {
+        this.episode = new Episode(response);
+        this.isLoading = false;
+      });
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      const id: number = +params.get("id");
+      if (id) {
+        this.isLoading = true;
+        this.getEpisode(id);
+      }
+    });
+  }
 }
