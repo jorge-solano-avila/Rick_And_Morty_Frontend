9f8251555913a82ed205ae243c04ef6fe55ad1a1
Jorge Solano <jorgesolavila@gmail.com>
2019-03-13 11:18:28 -0500

    Feat: add episode component
    Add initial component

---
+++ src/app/components/episode/episode.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-episode",
+  templateUrl: "./episode.html",
+  styleUrls: ["./episode.scss"]
+})
+export class EpisodeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
