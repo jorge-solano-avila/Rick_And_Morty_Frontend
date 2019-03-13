2acc8673aa46e2c70f0c1954d364b8004b620306
Jorge Solano <jorgesolavila@gmail.com>
2019-03-13 10:57:12 -0500

    Feat: update app routing module
    Add seasons route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+import { SeasonComponent } from "./components/season/season";
+
+const routes: Routes = [
+  { path: "seasons/:id", component: SeasonComponent },
+  { path: "", redirectTo: "/seasons/1", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
