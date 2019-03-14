38d0c215b748e6faa30fa475abe14c98b8f55102
Jorge Solano <jorgesolavila@gmail.com>
2019-03-13 19:02:14 -0500

    Feat: update app routing module
    Add EpisodeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { SeasonComponent } from "./components/season/season";
+import { EpisodeComponent } from "./components/episode/episode";
 
 const routes: Routes = [
   { path: "seasons/:id", component: SeasonComponent },
+  { path: "episodes/:id", component: EpisodeComponent },
   { path: "", redirectTo: "/seasons/1", pathMatch: "full" }
 ];
 
