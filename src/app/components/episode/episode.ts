acda0417cf4f803fcfc457992467764d7de8b9c7
Jorge Solano <jorgesolavila@gmail.com>
2019-03-13 20:46:12 -0500

    Feat: update episode component
    Show episode detail

--- src/app/components/episode/episode.ts
+++ src/app/components/episode/episode.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, ParamMap } from "@angular/router";
 
 import { EpisodeData, Episode } from "src/app/models/episode/episode";
-import { EpisodeService } from "../../services/episode/episode";
-import { AppGlobal } from "../../app.global";
+import { CharacterData, Character } from "src/app/models/character/character";
+import { EpisodeService } from "src/app/services/episode/episode";
+import { CharacterService } from "src/app/services/character/character";
 
 @Component({
   selector: "app-episode",
@@ -11,15 +12,20 @@ import { AppGlobal } from "../../app.global";
   styleUrls: ["./episode.scss"]
 })
 export class EpisodeComponent implements OnInit {
-  episode: Episode;
   isLoading: boolean;
+  charactersIsLoading: boolean;
+  episode: Episode;
+  characters: Array<Character>;
 
   constructor(
     private route: ActivatedRoute,
-    private episodeService: EpisodeService
+    private episodeService: EpisodeService,
+    private characterService: CharacterService
   ) {
-    this.episode = null;
     this.isLoading = true;
+    this.charactersIsLoading = true;
+    this.episode = null;
+    this.characters = [];
   }
 
   private getEpisode(id: number): void {
@@ -27,6 +33,21 @@ export class EpisodeComponent implements OnInit {
       .subscribe((response: EpisodeData) => {
         this.episode = new Episode(response);
         this.isLoading = false;
+        this.charactersIsLoading = true;
+        this.getCharacters();
+      });
+  }
+
+  private getCharacters(): void {
+    const charactersIds: Array<number> = this.episode.characters.map((charactersURLs: string) => {
+      const parts: Array<string> = charactersURLs.split("/");
+
+      return +parts[parts.length - 1];
+    });
+    this.characterService.getAllByIds(charactersIds)
+      .subscribe((response: Array<CharacterData>) => {
+        this.characters = response.map((characterData: CharacterData) => new Character(characterData));
+        this.charactersIsLoading = false;
       });
   }
 
